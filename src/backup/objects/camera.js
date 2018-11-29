af1ccc9527fa0c193f4ff5ebcd78d598b13b09a8
terrykingcha <terrykingcha@gmail.com>
2018-11-29 23:10:51 +0800

    del

--- src/backup/objects/camera.js
+++
@@ -1,5 +0,0 @@
-define(function() {
-    var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.001, 10000);
-    camera.name = 'camera';
-    return camera;
-});
--- src/backup/objects/renderer.js
+++
@@ -1,9 +0,0 @@
-define(function(scene) {
-    var renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('canvas')
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000, 1);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    return renderer;
-});
